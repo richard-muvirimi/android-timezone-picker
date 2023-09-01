0df8a71f3aaffe4c56fcefcd2d4fe55ba9a14a4b
Richard Muvirimi <richard@tyganeutronics.com>
2023-09-01 12:24:27 +0200

    Update Dependencies, Release 1.0.1

--- library/src/main/java/com/tyganeutronics/timezonepicker/TimeZonePickerAppCompatFragment.java
+++ library/src/main/java/com/tyganeutronics/timezonepicker/TimeZonePickerAppCompatFragment.java
@@ -2,5 +2,6 @@ package com.tyganeutronics.timezonepicker;
 
 public class TimeZonePickerAppCompatFragment extends TimeZonePickerBaseFragment {
 
+    public final static String TAG = "TimeZonePickerAppCompatFragment";
 
 }
